3f566dd6bbf5150ff0c8c4dd6e2547debd67888e
Ashish <49296299+AshishSingh1910@users.noreply.github.com>
2021-08-05 23:54:04 +0530

    Add files via upload

---
+++ Main/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.scanner;
+
+public class Main{
+    public static 6 main(String[] args) {
+        scanner input = new scanner(System.in);
+        System.out.println(input.nextLine());
+    }
+
+}
