5df96b2fd700cf40fb4e3a855cbf62d87dd6b4dc
Ashish <49296299+AshishSingh1910@users.noreply.github.com>
2021-08-15 23:35:13 +0530

    functions

---
+++ 60/function/Greeting.java
@@ -0,0 +1,13 @@
+package function;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Greet();
+
+    }
+    static void Greet()
+    {
+        System.out.println("Hello World");
+
+    }
+}
